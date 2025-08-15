a5accff2400877337def4bb573b2dc23554deae1
Gamidu Welikanna <gamidukaveen@gmail.com>
2025-08-15 10:12:14 +0530

    15th August - Making the frontend

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -11,6 +11,16 @@ const Player = () => {
           <p>{songsData[0].desc.slice(0,12)}</p>
         </div>
       </div>
+      <div className='flex flex-col items-cemter gap-1 m-auto'>
+        <div className='flex gap-4'>
+          <img className='w-4 cursor-pointer' src={assets.shuffle_icon} alt='shuffle' />
+          <img className='w-4 cursor-pointer' src={assets.prev_icon} alt='previous' />
+          <img className='w-4 cursor-pointer' src={assets.play_icon} alt='play' />
+          <img className='w-4 cursor-pointer' src={assets.next_icon} alt='next' />
+          <img className='w-4 cursor-pointer' src={assets.loop_icon} alt='loop' />
+        </div>
+
+      </div>
       {/* You can add play/pause controls here */}
     </div>
   );
