57685d5a50cca5cdf24a9acbf250b9b675050a7e
Gamidu Welikanna <gamidukaveen@gmail.com>
2025-08-15 10:44:00 +0530

    15th August - Making the frontend

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -11,7 +11,7 @@ const Player = () => {
           <p>{songsData[0].desc.slice(0,12)}</p>
         </div>
       </div>
-      <div className='flex flex-col items-cemter gap-1 m-auto'>
+      <div className='flex flex-col items-center gap-1 m-auto'>
         <div className='flex gap-4'>
           <img className='w-4 cursor-pointer' src={assets.shuffle_icon} alt='shuffle' />
           <img className='w-4 cursor-pointer' src={assets.prev_icon} alt='previous' />
@@ -20,7 +20,27 @@ const Player = () => {
           <img className='w-4 cursor-pointer' src={assets.loop_icon} alt='loop' />
         </div>
 
+        <div className='flex items-center gap-5'>
+          <p>1:06</p>
+          <div className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
+            <hr className='h-1 border-none w-20 bg-green-800 rounded-full'></hr>
+          </div>
+          <p>3:20</p>
+        </div>
       </div>
+
+      <div className='hidden lg:flex items-center gap-2 opacity-75'>
+        <img className='w-4 ' src={assets.plays_icon} alt='' />
+        <img className='w-4 ' src={assets.mic_icon} alt='' />
+        <img className='w-4 ' src={assets.queue_icon} alt='' />
+        <img className='w-4 ' src={assets.speaker_icon} alt='' />
+        <img className='w-4 ' src={assets.volume_icon} alt='' />
+        <img className='w-4 ' src={assets.mini_player_icon} alt='' />
+        <img className='w-4 ' src={assets.zoom_icon} alt='' />
+
+      </div>
+
+      
       {/* You can add play/pause controls here */}
     </div>
   );
