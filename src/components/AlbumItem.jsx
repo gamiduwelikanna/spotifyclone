e59f9410871cff1d3bec48f12d6a6189085cca62
Gamidu Welikanna <gamidukaveen@gmail.com>
2025-08-16 16:42:38 +0530

    16th August 2025 - Making the frontend

--- src/components/AlbumItem.jsx
+++ src/components/AlbumItem.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const AlbumItem = ({ name, desc, id, image }) => {
+
+    const navigate = useNavigate()
+
   return (
-    <div className="min-w-[200px] max-w-[200px] bg-[#181818] p-4 rounded-lg cursor-pointer hover:bg-[#282828] mr-4">
+    <div onClick={()=>navigate(`/album/${id}`)} className="min-w-[200px] max-w-[200px] bg-[#181818] p-4 rounded-lg cursor-pointer hover:bg-[#282828] mr-4">
       <img src={image} alt={name} className="w-full rounded-lg mb-4" />
       <p className="text-white font-bold mb-2">{name}</p>
       <p className="text-white text-sm">{desc}</p>
