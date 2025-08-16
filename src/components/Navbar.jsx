a175e1a2083742a950580ae507f4f71efe7a9dd0
Gamidu Welikanna <gamidukaveen@gmail.com>
2025-08-16 13:47:02 +0530

    16th August 2025 - Making the frontend

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,8 @@ import { assets } from '../assets/frontend-assets/assets';
 
 const Navbar = () => {
   return (
-    <div className='flex items-center gap-4 w-full'>
+    <>
+    <div className='flex items-center justify-between w-full'>
       <div className='flex gap-2'>
         <button className='rounded-full bg-[#0A0A0A] p-2'>
           <img src={assets.arrow_left} alt="Previous" className='w-5 h-5' />
@@ -12,8 +13,18 @@ const Navbar = () => {
           <img src={assets.arrow_right} alt="Next" className='w-5 h-5' />
         </button>
       </div>
-      {/* Add other navbar items here */}
+      <div className='flex items-center  gap-4'>
+        <p className='bg-white text-black text-[18px] px-4 px-4 rounded-full font-semibold hover:scale-105'>Explore Premium</p>
+        <p className='bg-black text-white text-[18px] px-4 px-5 rounded-full font-semibold hover:scale-105'>Install App</p>
+        <p className='bg-green-500 text-black w-6 h-6 font-semibold rounded-full flex items-center justify-center'>G</p>
+      </div>
     </div>
+    <div className='flex items-center gap-2 mt-4'>
+      <p className='bg-white text-black text-[18px] px-4 px-4 rounded-full font-semibold hover:scale-105'>All</p>
+      <p className='bg-black text-white text-[18px] px-4 px-4 rounded-full font-semibold hover:scale-105'>Music</p>
+    </div>
+    </>
+    
   );
 };
 
