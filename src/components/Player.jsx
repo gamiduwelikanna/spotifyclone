3bcbd0b902002d9e2d81faee4171b3639e4505a6
Gamidu Welikanna <gamidukaveen@gmail.com>
2025-08-19 17:15:27 +0530

    19th August 2025 - Making the frontend

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,53 +1,73 @@
 import React, { useContext } from 'react';
 import { assets, songsData } from '../assets/frontend-assets/assets';
-import { PlayerContext } from '../context/PlayerContext';  // Fixed case sensitivity
+import { PlayerContext } from '../context/PlayerContext';
 
 const Player = () => {
-  const { seekBar, seekBg, playStatus, play, pause } = useContext(PlayerContext); // Fixed case sensitivity
+  const { seekBar, seekBg, playStatus, play, pause } = useContext(PlayerContext);
 
   return (
-    <div className='h-[10%] bg-black flex justify-between items-center text-white px-4'>
+    <div className='h-[90px] bg-black flex justify-between items-center text-white px-4'>
       <div className='hidden lg:flex items-center gap-4'>
-        <img className='w-12' src={songsData[0].image} alt='' />
+        <img className='w-12' src={songsData[0].image} alt={songsData[0].name} />
         <div>
           <p>{songsData[0].name}</p>
-          <p>{songsData[0].desc.slice(0,12)}</p>
+          <p className='text-gray-400'>{songsData[0].desc.slice(0, 12)}</p>
         </div>
       </div>
+
       <div className='flex flex-col items-center gap-1 m-auto'>
         <div className='flex gap-4'>
-          <img className='w-4 cursor-pointer' src={assets.shuffle_icon} alt='shuffle' />
-          <img className='w-4 cursor-pointer' src={assets.prev_icon} alt='previous' />
-          <img onClick={play} className='w-4 cursor-pointer' src={assets.play_icon} alt='play' />
-          <img onClick={pause} className='w-4 cursor-pointer' src={assets.pause_icon} alt='play' />
-          <img className='w-4 cursor-pointer' src={assets.next_icon} alt='next' />
-          <img className='w-4 cursor-pointer' src={assets.loop_icon} alt='loop' />
+          <img className='w-4 cursor-pointer hover:scale-110' src={assets.shuffle_icon} alt='shuffle' />
+          <img className='w-4 cursor-pointer hover:scale-110' src={assets.prev_icon} alt='previous' />
+          {playStatus ? (
+            <img 
+              className='w-4 cursor-pointer hover:scale-110' 
+              src={assets.pause_icon} 
+              alt='pause'
+              onClick={pause}
+            />
+          ) : (
+            <img 
+              className='w-4 cursor-pointer hover:scale-110' 
+              src={assets.play_icon} 
+              alt='play'
+              onClick={play}
+            />
+          )}
+          <img className='w-4 cursor-pointer hover:scale-110' src={assets.next_icon} alt='next' />
+          <img className='w-4 cursor-pointer hover:scale-110' src={assets.loop_icon} alt='loop' />
         </div>
 
         <div className='flex items-center gap-5'>
-          <p>1:06</p>
-          <div ref={seekBg} className='w-[60vw] max-w-[500px] bg-gray-300 rounded-full cursor-pointer'>
-            <hr ref={seekBar} className='h-1 border-none w-20 bg-green-800 rounded-full'></hr>
+          <p className='text-xs text-gray-400'>1:06</p>
+          <div 
+            ref={seekBg} 
+            className='w-[60vw] max-w-[500px] h-1 bg-gray-600 rounded-full cursor-pointer'
+          >
+            <div 
+              ref={seekBar} 
+              className='h-full bg-white hover:bg-green-500 rounded-full transition-all'
+              style={{ width: '20%' }}
+            ></div>
           </div>
-          <p>3:20</p>
+          <p className='text-xs text-gray-400'>3:20</p>
         </div>
       </div>
 
-      <div className='hidden lg:flex items-center gap-2 opacity-75'>
-        <img className='w-4 ' src={assets.plays_icon} alt='' />
-        <img className='w-4 ' src={assets.mic_icon} alt='' />
-        <img className='w-4 ' src={assets.queue_icon} alt='' />
-        <img className='w-4 ' src={assets.speaker_icon} alt='' />
-        <img className='w-4 ' src={assets.volume_icon} alt='' />
-        <div className='w-20 bg-slate-50 h-1 rounded'></div>
-        <img className='w-4 ' src={assets.mini_player_icon} alt='' />
-        <img className='w-4 ' src={assets.zoom_icon} alt='' />
-
+      <div className='hidden lg:flex items-center gap-2'>
+        <img className='w-4 cursor-pointer hover:opacity-80' src={assets.plays_icon} alt='playlist' />
+        <img className='w-4 cursor-pointer hover:opacity-80' src={assets.mic_icon} alt='microphone' />
+        <img className='w-4 cursor-pointer hover:opacity-80' src={assets.queue_icon} alt='queue' />
+        <img className='w-4 cursor-pointer hover:opacity-80' src={assets.speaker_icon} alt='speaker' />
+        <img className='w-4 cursor-pointer hover:opacity-80' src={assets.volume_icon} alt='volume' />
+        <div className='w-20 bg-gray-600 h-1 rounded-full'>
+          <div className='h-full w-1/2 bg-white hover:bg-green-500 rounded-full'></div>
+        </div>
+        <img className='w-4 cursor-pointer hover:opacity-80' src={assets.mini_player_icon} alt='mini player' />
+        <img className='w-4 cursor-pointer hover:opacity-80' src={assets.zoom_icon} alt='zoom' />
       </div>
-
-      
     </div>
   );
-}
+};
 
 export default Player;
